f6a1495d2831d46c943da918ab1251eab2be8ec8
aryanbansal499 <42020335+aryanbansal499@users.noreply.github.com>
2019-09-22 20:12:15 +1000

    added the LoginPage file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'provider.dart';
 import 'auth.dart';
-import 'validation.dart';
+import 'LoginPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,7 +31,7 @@ class MyHomePage extends StatelessWidget {
           if (loggedIn == true) {
             return HomePage();
           } else {
-            return LoginPage();
+            return new LoginPage();
           }
         }
         return CircularProgressIndicator();
@@ -69,151 +69,5 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
 
-class _LoginPageState extends State<LoginPage> {
-  final formKey = GlobalKey<FormState>();
 
-  String _email, _password;
-  FormType _formType = FormType.login;
-
-  bool validate() {
-    final form = formKey.currentState;
-    form.save();
-    if (form.validate()) {
-      form.save();
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  void submit() async {
-    if (validate()) {
-      try {
-        final auth = Provider.of(context).auth;
-        if (_formType == FormType.login) {
-          String userId = await auth.signInWithEmailAndPassword(
-            _email,
-            _password,
-          );
-
-          print('Signed in $userId');
-        } else {
-          String userId = await auth.createUserWithEmailAndPassword(
-            _email,
-            _password,
-          );
-
-          print('Registered in $userId');
-        }
-      } catch (e) {
-        print(e);
-      }
-    }
-  }
-
-  void switchFormState(String state) {
-    formKey.currentState.reset();
-
-    if (state == 'register') {
-      setState(() {
-        _formType = FormType.register;
-      });
-    } else {
-      setState(() {
-        _formType = FormType.login;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Form Page'),
-      ),
-      resizeToAvoidBottomPadding: false,
-      body: Center(
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: buildInputs() + buildButtons(),
-          ),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> buildInputs() {
-    return [
-      TextFormField(
-        validator: EmailValidator.validate,
-        decoration: InputDecoration(labelText: 'Email'),
-        onSaved: (value) => _email = value,
-      ),
-      TextFormField(
-        validator: PasswordValidator.validate,
-        decoration: InputDecoration(labelText: 'Password'),
-        obscureText: true,
-        onSaved: (value) => _password = value,
-      ),
-    ];
-  }
-
-  List<Widget> buildButtons() {
-    if (_formType == FormType.login) {
-      return [
-        RaisedButton(
-          child: Text('Login'),
-          color: Colors.blueAccent,
-          onPressed: submit,
-        ),
-        FlatButton(
-          child: Text('Register Account'),
-          color: Colors.teal,
-          onPressed: () {
-            switchFormState('register');
-          },
-        ),
-        Divider(
-          height: 50.0,
-        ),
-        FlatButton(
-          child: Text("Sign in with Google"),
-          color: Colors.lightGreen,
-          onPressed: () async {
-            try {
-              final _auth = Provider.of(context).auth;
-              final id = await _auth.signInWithGoogle();
-              print('signed in with google $id');
-            } catch (e) {
-              print(e);
-            }
-          },
-        ),
-      ];
-    } else {
-      return [
-        RaisedButton(
-          child: Text('Create Account'),
-          color: Colors.blueAccent,
-          onPressed: submit,
-        ),
-        FlatButton(
-          child: Text('Go to Login'),
-          color: Colors.teal,
-          onPressed: () {
-            switchFormState('login');
-          },
-        )
-      ];
-    }
-  }
-}
