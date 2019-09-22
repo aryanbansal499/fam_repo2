01c7529f81950a5e51751707b3f44b622741bcf7
Yen <jqmac@ymail.com>
2019-09-23 00:39:42 +1000

    Added Profile and Artefact classes & widgets

---
+++ lib/ArtefactItem.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ArtefactItem extends StatelessWidget {
+
+  // artefact is passed as argument by onTap from Home view
+  final Image image;
+  final String name;
+  final String family;
+  final String description; // Can be audio as well
+  final DateTime date;
+  final String tags;
+  final String comments; // As its own class
+
+  ArtefactItem(this.image, this.name, this.family, this.description,
+      this.date, this.tags, this.comments);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return image;
+  }
+}
---
+++ lib/Profile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+
+  final String name;
+  final String username;
+  final String email;
+  final String profileDesc;
+  final bool isPrivate;
+  final Image dp;
+
+  Profile(this.name, this.username, this.email, this.profileDesc, this.isPrivate, this.dp);
+
+  @override
+  Widget build(BuildContext context) {
+    return dp;
+  }
+}
