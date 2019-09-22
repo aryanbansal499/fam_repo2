de8fd3c6e19fc97968e2954533e7dd20b1e2fafd
aryanbansal499 <42020335+aryanbansal499@users.noreply.github.com>
2019-09-23 03:00:50 +1000

    upload page added, access pics from the phone

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,13 +1,22 @@
+import 'package:fam_repo2/Upload.dart';
 import 'package:flutter/material.dart';
 import 'auth.dart';
 import 'provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+
+   _HomePageState createState() => _HomePageState();
+}
+    
+class _HomePageState extends State<HomePage> 
+{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome Page'),
+        title: Text('DashBoard'),
+        centerTitle: true,
         actions: <Widget>[
           FlatButton(
             child: Text("Sign Out"),
@@ -22,11 +31,71 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: Container(
-        child: Center(
-          child: Text('Welcome'),
+      body: new Container
+        (
+
         ),
-      ),
+        bottomNavigationBar : new BottomAppBar
+          (
+            color : Colors.deepOrange,
+            child: new Container
+            (
+              margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+              child: new Row 
+              (
+                mainAxisAlignment : MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+
+                children: <Widget>
+                [
+                  new IconButton(
+                    icon: new Icon(Icons.add_a_photo),
+                    iconSize: 35,
+                    color: Colors.white,
+
+                    onPressed: ()
+                    {
+                      Navigator.push
+                      (
+                        context,
+                        MaterialPageRoute(builder: (context)
+                        {
+                          return new UploadPage();
+
+                        })
+
+                      );
+                    },
+                  ),
+
+
+                  new IconButton(
+                    icon: new Icon(Icons.grid_on),
+                    iconSize: 35,
+                    color: Colors.brown,
+                    onPressed: null,
+                  ),
+
+                  new IconButton(
+                    icon: new Icon(Icons.mic),
+                    iconSize: 35,
+                    color: Colors.brown,
+                    onPressed: null,
+                  ),
+
+                  new IconButton(
+                    icon: new Icon(Icons.thumb_down),
+                    iconSize: 35,
+                    color: Colors.brown,
+                    onPressed: null,
+                  ),
+                ]
+
+              )
+
+            ) 
+          )
     );
   }
+  
 }
---
+++ lib/Upload.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+
+
+
+class UploadPage extends StatefulWidget
+{
+  
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _UploadPageState();
+  }
+}
+
+class _UploadPageState extends State<UploadPage>{
+
+  File sampleImage;
+  String _myValue;
+
+  final formKey = new GlobalKey<FormState>(); 
+
+  Future getImage() async
+  {
+    var tempImage = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+     sampleImage = tempImage; 
+    });
+  }
+
+
+  bool validateAndSave()
+  {
+    final form = formKey.currentState;
+    if (form.validate())
+    {
+      form.save();
+      return true;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar
+      (
+        title: new Text("Upload Page"),
+        centerTitle: true,
+      ),
+
+      body: new Center 
+      (
+        child: sampleImage == null ? Text("Select an Image"): enableUpload(),
+        ),
+     
+     floatingActionButton: new FloatingActionButton
+     (
+       onPressed: getImage,
+       tooltip: 'Add Image',
+       child : new Icon(Icons.add_a_photo), 
+     ),
+    );
+  }
+  
+
+
+  Widget enableUpload()
+  {
+    return Container
+    (
+      child: new Form
+      (
+        key: formKey,
+      child: Column
+      (
+        children: <Widget>
+        [
+          Image.file(sampleImage,height: 310.0,width: 600.0,),
+          
+          SizedBox(height: 15.0,),
+          RaisedButton
+          (
+            elevation: 10.0,
+            child: Text("Add a New Post"),
+            textColor: Colors.white,
+            color: Colors.deepOrange,
+
+            onPressed: validateAndSave,
+          ),
+          TextFormField
+          (
+            decoration: new InputDecoration(labelText: 'Description'),
+            validator: (value)
+            {
+              return value.isEmpty? 'Blod Description is required' : null;
+
+
+            },
+            onSaved: (value)
+            {
+               return _myValue = value;
+            },
+          )
+      
+        ],
+      ),
+      ),
+
+    );
+  }
+}
