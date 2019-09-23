7b3609d4db99d63e905000f57abe7613be71c859
aryanbansal499 <42020335+aryanbansal499@users.noreply.github.com>
2019-09-23 16:28:47 +1000

    cleaned the code running again

--- lib/FireStore.dart
+++
@@ -1,3 +0,0 @@
-import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'User.dart';
--- lib/Upload.dart
+++ lib/Upload.dart
@@ -5,7 +5,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 
