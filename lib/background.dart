a3740ca2b05b2a25c3c6f61a6cc1af5ca385519a
Jorjilou Reyes <jorjilou.reyes@gmail.com>
2019-09-24 00:50:46 +1000

    background.dart added to add background of all screens

---
+++ lib/background.dart
@@ -0,0 +1,21 @@
+/**
+ * To use this class, you must wrap it in a stack widget in body:,
+ *  first stack: background, second is the column widget etc.
+ */
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+
+    return new Center(
+      child: new Image.asset(
+              'assets/images/bg.png',
+              width: screenSize.width,
+              height: screenSize.height,
+              fit: BoxFit.fitHeight)
+      );
+  }
+}
