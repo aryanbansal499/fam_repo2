ba25711bcbf27a781b66596e4579e7c712450afa
Jorjilou Reyes <jorjilou.reyes@gmail.com>
2019-10-07 18:43:40 +1100

    got drop down button feature to work. will now see if i can put it inside a line

--- lib/drop_down_button.dart
+++ lib/drop_down_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class YearList extends StatefulWidget {
   @override
@@ -6,36 +7,60 @@ class YearList extends StatefulWidget {
 }
 
 class _YearListState extends State<YearList> {
-  String dropdownValue = 'One';
+
+  String _dropdownValue;
+  static const int START_YEAR = 2019;
+  static const int LAST_YEAR = 1990;
+  List<String> decades = [];
+  List<DropdownMenuItem> listDrop = [];
+
+//  @override
+//  void initState() {
+//    // TODO: implement initState
+//    getYearList();
+//  }
+
+//
+
+  void
+  loadData() {
+    //decades = [];
+    decades = new List();
+
+    for(int i = START_YEAR; i >= LAST_YEAR; i--) {
+      decades.add(i.toString());
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
-      return DropdownButton<String>(
-        value: dropdownValue,
-        icon: Icon(Icons.arrow_downward),
-        iconSize: 24,
-        elevation: 16,
-        style: TextStyle(
-            color: Colors.deepPurple
-        ),
-        underline: Container(
-          height: 2,
-          color: Colors.deepPurpleAccent,
-        ),
-        onChanged: (String newValue) {
-          setState(() {
-            dropdownValue = newValue;
-          });
-        },
-        items: <String>['One', 'Two', 'Free', 'Four']
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
+    Size screenSize = MediaQuery.of(context).size;
+    loadData();
+
+    return DropdownButton<String>(
+      value: _dropdownValue,
+      icon: Icon(Icons.arrow_downward, color: Colors.black,),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        width: screenSize.width,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          _dropdownValue = newValue;
+        });
+      },
+      hint: Text('Decade of Origin'),
+      items: decades.map((decade) {
+        return new DropdownMenuItem(
+              value: decade,
+              child: new Text(decade)
           );
-        }).toList(),
-      );
-
-
+      }).toList(),
+    );
   }
 }
